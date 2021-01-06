329cbd6ecb82cb7a2017a458aa8ece1ec77dd696
chadychaito <chady.chaito@dcomp.sor.ufscar.br>
2021-01-05 22:20:37 -0300

    Componente de editar formulário, falta estilização utilizando flex box para ajeitar os inputs; Falta adicionar botão de +/-;

--- frontend/src/pages/ProductDetail/components/EditForm/index.jsx
+++ frontend/src/pages/ProductDetail/components/EditForm/index.jsx
@@ -1,5 +1,57 @@
+import {
+  InputGroup,
+  Label,
+  Input,
+  Stock,
+  Price,
+  Dimensions,
+  DimensionItem,
+} from "./styles";
+
 const EditForm = () => {
-  return <h1>Formulário</h1>;
+  return (
+    <>
+      <Stock>
+        <InputGroup>
+          <Label>Estoque</Label>
+          <Input type="number" />
+        </InputGroup>
+        <div>+/-</div>
+      </Stock>
+      <Price>
+        <InputGroup>
+          <Label>Preço de venda</Label>
+          <Input type="number" />
+        </InputGroup>
+        {/* <InputGroup>
+          <Label>Preço promocional</Label>
+          <Input type="number" />
+        </InputGroup> */}
+      </Price>
+      <Dimensions>
+        <DimensionItem>
+          <Label>Peso</Label>
+          <Input type="number" />
+          <Label>kg</Label>
+        </DimensionItem>
+        <DimensionItem>
+          <Label>Altura</Label>
+          <Input type="number" />
+          <Label>cm</Label>
+        </DimensionItem>
+        <DimensionItem>
+          <Label>Largura</Label>
+          <Input type="number" />
+          <Label>cm</Label>
+        </DimensionItem>
+        <DimensionItem>
+          <Label>Profundidade</Label>
+          <Input type="number" />
+          <Label>cm</Label>
+        </DimensionItem>
+      </Dimensions>
+    </>
+  );
 };
 
 export default EditForm;
---
+++ frontend/src/pages/ProductDetail/components/EditForm/styles.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const InputGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Label = styled.label`
+  font-family: "Rubik", sans-serif;
+  font-size: 15px;
+  font-weight: 400px;
+  color: #212b36;
+  line-height: 20px;
+  margin-bottom: 4px;
+`;
+
+export const Input = styled.input`
+  border: 1px solid #c4cdd5;
+  border-radius: 3px;
+  height: 36px;
+`;
+
+export const Stock = styled.div`
+  display: flex;
+  align-items: flex-end;
+`;
+
+export const Price = styled.div`
+  width: 100%;
+`;
+
+export const Dimensions = styled.div``;
+
+export const DimensionItem = styled.div`
+  border-bottom: 1px solid #dfe3e8;
+  padding: 18px 0px;
+`;
